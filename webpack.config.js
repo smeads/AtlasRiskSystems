e522a52c1d6cffac7a072a1b1eed7bd33a55d4cc
Steven Meads <sbmeads@gmail.com>
2017-07-18 22:55:07 -0700

    (config) webpack for react with hot module reloading

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    // activate HMR for React
+    'react-hot-loader/patch',
+    // bundle the client for webpack-dev-server and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for hot reloading only- means to only hot reload for successful updates
+    'webpack/hot/only-dev-server',
+    // the entry point of our app
+    './src/index.js'
+  ],
+  output: {
+    // the output bundle
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'build'),
+    // necessary for HMR to know where to load the hot update chunks
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    // enable HMR on the server
+    hot: true,
+    // match the output path
+    contentBase: resolve(__dirname, 'build'),
+    // match the output `publicPath`
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [ 'babel-loader', ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader?modules', ],
+      },
+    ],
+  },
+  plugins: [
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+      // minify: {
+      //   collapseWhitespace: true,
+      // },
+    }),
+  ],
+};
