59abd47166bb8efc928c678727424a8487cca418
Steven Meads <sbmeads@gmail.com>
2017-07-20 18:37:00 -0700

    (add) config for bootstrap, sass, images, fonts

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const { resolve } = require('path');
 const webpack = require('webpack');
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
+
+const isProd = process.env.NODE_ENV === 'production'; // true or false
+const cssDev = [
+  'style-loader',
+  'css-loader?sourceMap',
+  'sass-loader',
+  {
+    loader: 'sass-resources-loader',
+    options: {
+      // Provide path to the file with resources
+      resources: ['./src/resources.scss'],
+    },
+  },
+];
+
+const cssProd = ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: [
+    'css-loader',
+    'sass-loader',
+    {
+      loader: 'sass-resources-loader',
+      options: {
+        // Provide path to the file with resources
+        resources: ['./src/resources.scss'],
+      },
+    },
+  ],
+  publicPath: '',
+});
+
+const cssConfig = isProd ? cssProd : cssDev;
+
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
   entry: {
@@ -10,14 +47,15 @@ module.exports = {
       'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
       'webpack/hot/only-dev-server', // bundle the client for hot reloading only- means to only hot reload for successful updates
       './src/index.js' // the entry point of our app
-    ]
+    ],
+    bootstrap: bootstrapConfig // vendor entry point for (bootstrap)
   },
   output: {
     filename: 'js/[name].bundle.js', // the output bundle
     path: resolve(__dirname, 'build'),
     publicPath: '/' // necessary for HMR to know where to load the hot update chunks
   },
-  devtool: 'inline-source-map',
+  // devtool: 'source-map',
   devServer: {
     hot: true, // enable HMR on the server
     contentBase: resolve(__dirname, 'build'), // match the output path
@@ -26,17 +64,30 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader?modules', ],
-      },
-      {
         test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader'],
+        use: cssConfig,
       },
       {
         test: /\.(js|jsx)$/,
-        use: [ 'babel-loader', ],
+        use: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: ['file-loader?name=/images/[name].[ext]', 'image-webpack-loader?bypassOnDebug'],
+      },
+      {
+        test: /\.(woff2?)$/,
+        use: 'url-loader?limit=10000&name=/fonts/[name].[ext]'
+      },
+      {
+        test: /\.(ttf|eot)$/,
+        use: 'file-loader?name=/fonts/[name].[ext]'
+      },
+      // Bootstrap 3
+      {
+        test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+        use: 'imports-loader?jQuery=jquery'
       },
     ],
   },
@@ -50,6 +101,20 @@ module.exports = {
       // minify: {
       //   collapseWhitespace: true,
       // },
+    }),
+    new ExtractTextPlugin({
+      filename: './css/[name].css',
+      disable: !isProd,
+      allChunks: true,
+    }),
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(
+        resolve(__dirname, 'src/*.html')
+      ),
+      purifyOptions: {
+        whitelist: ['on-scroll', 'nav-pad'],
+      },
     }),
   ],
 };
